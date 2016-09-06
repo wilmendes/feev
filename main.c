b44898036e9f1539fb0db7b99c45e374d683a01a
Mendes <I852781@global.corp.sap>
2016-09-06 14:22:56 -0300

    finished

--- main.c
+++ main.c
@@ -36,6 +36,7 @@ Node* insertBegin(Node* first, Cliente client){
     novo->proximo = first;
     return novo;
 }
+
 void insertEnd(Node* first, Cliente client){
     Node* last;
     for(last = first;last->proximo != NULL; last = last->proximo);
@@ -44,9 +45,11 @@ void insertEnd(Node* first, Cliente client){
     novo->cliente = client;
     novo->proximo = NULL;
 }
+
 int isEmpty(Node* lst){
     return (lst == NULL);
 }
+
 void printList(Node* first){
     Node* iter;
     for(iter = first; iter!= NULL; iter = iter->proximo){
@@ -55,6 +58,84 @@ void printList(Node* first){
         printf("Nome: %s\n",(iter->cliente).nome);
         printf("Endereco: %p\n",iter);
     }
+}
+Node* findNode(Node* first, int search){
+    Node* iter;
+    for(iter = first; iter !=NULL; iter = iter->proximo){
+        if((iter->cliente).mat == search)
+            return iter;
+    }
+    return NULL;
+}
+
+Node* removeNode(Node* first, int search){
+    Node* previous = NULL;
+    Node* iter = first;
+
+    while(iter != NULL && (iter->cliente).mat != search){
+        previous = iter;
+        iter = iter->proximo;
+    }
+    if(iter == NULL){
+        return first;
+    }
+    if(previous == NULL){
+        first = iter->proximo;
+    }else{
+        previous->proximo = iter->proximo;
+    }
+    free(iter);
+    return first;
+}
+Node* listClear(Node* first){
+    while(first != NULL){
+        Node* temp = first->proximo;
+        free(first);
+        first = temp;
+    }
+    return first;
+}
+Node* insertAsc(Node* first, Cliente client){
+    Node* novo;
+    Node* previous = NULL;
+    Node* iter = first;
+
+    while(iter != NULL && (iter->cliente).mat < client.mat){
+        previous = iter;
+        iter = iter->proximo;
+    }
+    novo = malloc(sizeof(Node));
+    novo->cliente = client;
+
+    if(previous == NULL){
+        novo->proximo = first;
+        first = novo;
+    }else{
+        novo->proximo = previous->proximo;
+        previous->proximo = novo;
+    }
+    return first;
+}
+Node* insertDesc(Node* first, Cliente client){
+    Node* novo;
+    Node* previous = NULL;
+    Node* iter = first;
+
+    while(iter != NULL && (iter->cliente).mat <= client.mat){
+        previous = iter;
+        iter = iter->proximo;
+    }
+    novo = malloc(sizeof(Node));
+    novo->cliente = client;
+
+    if(previous == NULL){
+        first->proximo = novo;
+        novo->proximo = NULL;
+    }else{
+        novo->proximo = iter;
+        previous->proximo = novo;
+    }
+    return first;
 }
 
 int main(){
@@ -66,9 +147,20 @@ int main(){
     y.mat = 456;
     copyString("Hahaha", y.nome);
 
-    Node* list = listCreate();
-    list = insertBegin(list, w);
-    insertEnd(list, y);
+    Cliente z;
+    z.mat = 455;
+    copyString("Lalala", z.nome);
 
+    Node* list = listCreate();
+
+    list = insertBegin(list, y);
+    printList(list);
+
+    printf("\n\n----- Insert Ascending ----");
+    list = insertAsc(list, w);
+    printList(list);
+
+    printf("\n\n----- Insert Descending ----");
+    list = insertDesc(list, z);
     printList(list);
 }
